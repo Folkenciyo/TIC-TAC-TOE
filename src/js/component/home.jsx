d4a08aa2d02250c3b8289dce334bb7121d07b061
Folkenciyo <folkencillo@gmail.com>
2021-10-30 16:45:54 +0000

    pruebando!

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,24 +4,13 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import { position } from "dom-helpers";
 
-const solutions = [
-	[0, 1, 2],
-	[3, 4, 5],
-	[6, 7, 8],
-	[0, 3, 6],
-	[1, 4, 7],
-	[2, 5, 8],
-	[0, 4, 8],
-	[2, 4, 6]
-];
-
 const Home = () => {
 	const [turn, setTurn] = useState("👿");
 	const [board, setBoard] = useState(new Array(9)); //es el comparador de la partida
 
 	const changeTurn = () => {
 		setBoard();
-		let winner = isWinner(position);
+		let winner = calculateWinner(position);
 		if (!winner) {
 			if (turn == "👿") {
 				setTurn("👼");
@@ -31,8 +20,34 @@ const Home = () => {
 		}
 	};
 
-	const isWinner = board => {
-		//se recorre la solucion, si coinciden solution[A=B=C] retornamos 👿 o 👼, que son plenos ganadores
+	//Creacion de las casillas con un index dentro de un array vacio de  9 espacios
+	//para poder compararlas con la solucion.
+	const position = ["", "", "", "", "", "", "", "", ""];
+	const listOfBoxes = position.map((_, index) => {
+		return (
+			<Box
+				key={index.toString}
+				value={turn}
+				changeTurn={changeTurn}
+				ganador={gameStatus}
+			/>
+		);
+	});
+
+	//solution Function
+	const calculateWinner = board => {
+		const solutions = [
+			[0, 1, 2],
+			[3, 4, 5],
+			[6, 7, 8],
+			[0, 3, 6],
+			[1, 4, 7],
+			[2, 5, 8],
+			[0, 4, 8],
+			[2, 4, 6]
+		];
+		//se recorre la solucion, si coinciden solution[A=B=C] retornamos 👿 o 👼,
+		//que son plenos ganadores
 		for (let i = 0; i < solutions.length; i++) {
 			const [a, b, c] = solutions[i];
 			if (board[a] && board[a] === board[b] && board[a] === board[c]) {
@@ -42,36 +57,54 @@ const Home = () => {
 		}
 	};
 
+	//ganad@r
+	const theWinner = calculateWinner(listOfBoxes);
+
+	//Esta funcion es para determinar el empate, dado el caso
+	const isBoardFull = table => {
+		for (let i = 0; i < table.length; i++) {
+			if (table[i] == null) {
+				return false;
+			}
+		}
+		return true;
+	};
+
+	//Determina el estado del juego
+	const gameStatus = () => {
+		if (theWinner) {
+			return console.log(theWinner);
+		} else if (isBoardFull(listOfBoxes) === true) {
+			return "empate baby";
+		} else {
+			return console.log("next");
+		}
+	};
+
 	/* function restart() {
 		setBoard(Array(9).fill(null))  //restart the game
 	  } */
 
-	/* key={i} value={square} onClick={() => onClick(i) */
-
-	const position = ["", "", "", "", "", "", "", "", ""];
-	const listOfBoxes = position.map((_, index) => {
-		return (
-			<Box key={index.toString} value={turn} changeTurn={changeTurn} />
-		);
-	});
-
 	return (
 		<Container>
-			<Row>
-				{listOfBoxes[0]}
-				{listOfBoxes[1]}
-				{listOfBoxes[2]}
-			</Row>
-			<Row>
-				{listOfBoxes[3]}
-				{listOfBoxes[4]}
-				{listOfBoxes[5]}
-			</Row>
-			<Row>
-				{listOfBoxes[6]}
-				{listOfBoxes[7]}
-				{listOfBoxes[8]}
-			</Row>
+			<div className="game-status">{gameStatus()}</div>
+			<div>
+				<Row>
+					{listOfBoxes[0]}
+					{listOfBoxes[1]}
+					{listOfBoxes[2]}
+				</Row>
+				<Row>
+					{listOfBoxes[3]}
+					{listOfBoxes[4]}
+					{listOfBoxes[5]}
+				</Row>
+				<Row>
+					{listOfBoxes[6]}
+					{listOfBoxes[7]}
+					{listOfBoxes[8]}
+				</Row>
+			</div>
 		</Container>
 	);
 };
